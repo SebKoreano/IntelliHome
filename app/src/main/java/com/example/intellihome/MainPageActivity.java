3a1464da5177e5c876992f50b3df2b1e93c7d27a
luisfeliTEC-0603 <167169513+luisfeliTEC-0603@users.noreply.github.com>
2024-10-22 03:54:05 -0600

    Base Main Page

--- app/src/main/java/com/example/intellihome/MainPageActivity.java
+++ app/src/main/java/com/example/intellihome/MainPageActivity.java
@@ -1,21 +1,28 @@
 package com.example.intellihome;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.MenuItem;
 
-import androidx.appcompat.app.ActionBar;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
 import com.google.android.material.navigation.NavigationView;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-public class MainPageActivity extends AppCompatActivity {
+public class MainPageActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     private DrawerLayout drawerLayout;
     private NavigationView navigationView;
+    private BottomNavigationView bottomNavigationView;
     private Toolbar toolbar;
-
+    private SearchView searchView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,9 +32,51 @@ public class MainPageActivity extends AppCompatActivity {
         drawerLayout = findViewById(R.id.drawer_menu);
         navigationView = findViewById(R.id.nav_view);
         toolbar = findViewById(R.id.menu_toolbar);
+        searchView = findViewById(R.id.mainpage_search_bar);
+        bottomNavigationView = findViewById(R.id.bottom_nav);
 
+        navigationView.bringToFront();
         setSupportActionBar(toolbar);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open_nav_drawer, R.string.close_nav_drawer);
         toggle.syncState();
+
+        navigationView.setNavigationItemSelectedListener(this);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                return MainPageActivity.this.onNavigationItemSelected(menuItem);
+            }
+        });
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        int id = menuItem.getItemId();
+
+        // Lógica para el NavigationView
+        if (id == R.id.menu_profile) {
+            // Lógica para perfil
+        } else if (id == R.id.menu_settings) {
+            Intent intent = new Intent(MainPageActivity.this, ConfigActivity.class);
+            startActivity(intent);
+        } else if (id == R.id.menu_add_house) {
+            // Lógica para publicar casa
+        } else if (id == R.id.menu_control_house) {
+            // Lógica para monitorear viviendas
+        }
+
+        // Lógica para el BottomNavigationView
+        if (id == R.id.bottom_nav_home) {
+            Intent intent = new Intent(MainPageActivity.this, ConfigActivity.class);
+            startActivity(intent);
+            return true;
+        } else if (id == R.id.bottom_nav_search) {
+            // Lógica para buscar
+            return true;
+        }
+
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
