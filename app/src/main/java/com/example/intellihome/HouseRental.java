56551a6d4b3b9d3a845ca4cbf109ae879219c5bf
luisfeliTEC-0603 <167169513+luisfeliTEC-0603@users.noreply.github.com>
2024-10-24 06:59:14 -0600

    House Rental Dynamic Changes

--- app/src/main/java/com/example/intellihome/HouseRental.java
+++ app/src/main/java/com/example/intellihome/HouseRental.java
@@ -1,20 +1,23 @@
 package com.example.intellihome;
 
 import android.os.Bundle;
-
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
-import android.widget.Button;
-import android.widget.CheckBox;
-import android.widget.EditText;
-import android.widget.ImageView;
 import android.widget.TextView;
+
+import java.util.List;
 
 public class HouseRental extends AppCompatActivity {
 
-    TextView title;
+    private TextView title;
+    private TextView type;
+    private TextView vehicle;
+    private TextView description;
+    private TextView rooms;
+    private TextView money;
+    private TextView owner;
+    private TextView coords;
+    private TextView rules;
+    private TextView amenities;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,7 +25,34 @@ public class HouseRental extends AppCompatActivity {
         setContentView(R.layout.activity_house_rental);
 
         PropertyModule house = (PropertyModule) getIntent().getSerializableExtra("property");
+
         title = findViewById(R.id.property_title);
-        title.setText(house.getTitle());
+        type = findViewById(R.id.property_type);
+        vehicle = findViewById(R.id.property_vehicle);
+        description = findViewById(R.id.property_description);
+        rooms = findViewById(R.id.property_rooms);
+        money = findViewById(R.id.property_money);
+        owner = findViewById(R.id.property_owner);
+        coords = findViewById(R.id.property_coords);
+        rules = findViewById(R.id.property_rules);
+        amenities = findViewById(R.id.property_amenities);
+
+        // Set the information of the house
+        if (house != null) {
+            title.setText(house.getTitle());
+            type.setText(house.getType());
+            vehicle.setText(house.getVehicle());
+            description.setText(house.getDescription());
+            rooms.setText(house.getRooms());
+            money.setText(house.getMoney());
+            owner.setText(house.getOwner());
+            coords.setText("X: " + house.getX_coords() + ", Y: " + house.getY_coords());
+
+            // Joining rules and amenities with commas
+            List<String> rulesList = house.getRules();
+            List<String> amenitiesList = house.getAmenities();
+            rules.setText(rulesList != null ? String.join(", ", rulesList) : "No rules available");
+            amenities.setText(amenitiesList != null ? String.join(", ", amenitiesList) : "No amenities available");
+        }
     }
 }
