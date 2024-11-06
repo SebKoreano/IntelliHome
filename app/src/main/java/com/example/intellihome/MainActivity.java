2cd5397494144ca2da06ea2ee48b03609003fa05
luisfeliTEC-0603 <167169513+luisfeliTEC-0603@users.noreply.github.com>
2024-11-06 14:31:33 -0600

    Added PyWhatKit Lib

--- app/src/main/java/com/example/intellihome/MainActivity.java
+++ app/src/main/java/com/example/intellihome/MainActivity.java
@@ -80,7 +80,7 @@ public class MainActivity extends AppCompatActivity {
         // Iniciar el hilo para conectarse al servidor y recibir mensajes
         new Thread(() -> {
             try {
-                socket = new Socket("172.18.77.74", 3535); //192.168.18.206
+                socket = new Socket("172.18.77.88", 3535); //192.168.18.206
 
                 out = new PrintWriter(socket.getOutputStream(), true);
                 in = new Scanner(socket.getInputStream());
