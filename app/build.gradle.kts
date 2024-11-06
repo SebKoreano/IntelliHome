ee8c60f713cf94d5529d73b05d319d2b14b5bd39
luisfeliTEC-0603 <167169513+luisfeliTEC-0603@users.noreply.github.com>
2024-11-06 04:44:35 -0600

    Added Local Notification
    + Local Notification Class
    + Movement in-between Sign-Up and Log-In
    
    ! Still No WhatsApp
    ! Ask permitions for Notification without event
    ! House View Options
    ! "Merge" Everything

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,6 +40,7 @@ dependencies {
     implementation(libs.constraintlayout)
     implementation(libs.protolite.well.known.types)
     implementation(libs.play.services.maps)
+    implementation(libs.core)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
